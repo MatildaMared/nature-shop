990f8498512ce8b0467625a806914dee1f933cea
Matilda Mared <matildamared@live.se>
2022-02-15 13:33:56 +0100

    add tests for Footer component

--- client/src/components/Footer/Footer.test.tsx
+++ client/src/components/Footer/Footer.test.tsx
@@ -1,8 +1,36 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Footer from "./Footer";
 
 describe("Footer component", () => {
 	it("should render", () => {
 		render(<Footer />);
+	});
+
+	it("displays the correct text", () => {
+		render(<Footer />);
+
+		const text = "© Matilda Mared 2022";
+
+		const copy = screen.getByText(text);
+
+		expect(copy).toBeInTheDocument();
+	});
+
+	it("displays the correct number of links", () => {
+		render(<Footer />);
+
+		const links = screen.getAllByRole("link");
+
+		expect(links.length).toBe(3);
+	});
+
+	it("displays the correct links", () => {
+		render(<Footer />);
+
+		const links = screen.getAllByRole("link");
+
+		expect(links[0].textContent).toBe("GitHub");
+		expect(links[1].textContent).toBe("Mail");
+		expect(links[2].textContent).toBe("LinkedIn");
 	});
 });
