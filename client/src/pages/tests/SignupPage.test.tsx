24c1ab856403e4a8f1712c007ed70cba9725f754
Matilda Mared <matildamared@live.se>
2022-02-16 11:36:09 +0100

    add tests for SignupPage component

---
+++ client/src/pages/tests/SignupPage.test.tsx
@@ -0,0 +1,63 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import { UserContext } from "../../context/UserContext";
+import SignupPage from "../SignupPage";
+
+const context = {};
+
+const updateContext = jest.fn();
+
+const dummyContext = [context, updateContext];
+
+describe("LoginPage component", () => {
+	it("renders without crashing", () => {
+		render(
+			<UserContext.Provider value={dummyContext}>
+				<BrowserRouter>
+					<SignupPage />
+				</BrowserRouter>
+			</UserContext.Provider>
+		);
+	});
+
+	it("displays the correct title", () => {
+		render(
+			<UserContext.Provider value={dummyContext}>
+				<BrowserRouter>
+					<SignupPage />
+				</BrowserRouter>
+			</UserContext.Provider>
+		);
+
+		const title = screen.getByRole("heading", { name: "Sign up" });
+		expect(title).toBeInTheDocument();
+	});
+
+	it("displays a form", () => {
+		render(
+			<UserContext.Provider value={dummyContext}>
+				<BrowserRouter>
+					<SignupPage />
+				</BrowserRouter>
+			</UserContext.Provider>
+		);
+
+		const nameInput = screen.getByLabelText(/Name/i);
+		expect(nameInput).toBeInTheDocument();
+
+		const emailInput = screen.getByLabelText(/Email/i);
+		expect(emailInput).toBeInTheDocument();
+
+		const passwordInput = screen.getByLabelText("Password");
+		expect(passwordInput).toBeInTheDocument();
+
+		const passwordConfirmInput = screen.getByLabelText("Confirm password");
+		expect(passwordConfirmInput).toBeInTheDocument();
+
+		const addressContainer = screen.getByText(/Address/i);
+		expect(addressContainer).toBeInTheDocument();
+
+		const button = screen.getByRole("button", { name: "Sign up" });
+		expect(button).toBeInTheDocument();
+	});
+});
